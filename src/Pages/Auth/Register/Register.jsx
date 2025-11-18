0472998e8e795ce0db16a3f4f0477676b51c62c2
Istheaksayem <istheakahmed9345@gmail.com>
2025-11-18 09:22:27 +0600

    added a React Hook Form & Create Registration Page

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleRegistration = (data) => {
+        console.log("After register", data)
+    }
     return (
         <div>
-            Register
+            <form onSubmit={handleSubmit(handleRegistration)}>
+                <fieldset className="fieldset">
+                    <label className="label">Email</label>
+                    <input type="email" {...register("email", { required: true })} className="input" placeholder="Email" />
+
+                    {errors.email?.type === "required" && <p className='text-red-500'>Email is required</p>}
+
+                    {/* password */}
+                    <label className="label">Password</label>
+                    <input type="password"  {...register("password", {
+                        required: true,
+                        minLength: 6,
+                    })} className="input" placeholder="Password" />
+
+
+                    {
+                        errors.password?.type === "required" && <p className='text-red-500'>password is required</p>
+                    }
+                    {
+                        errors.password?.type === "minLength" && <p className='text-red-500'>password must be 6 characters</p>
+                    }
+
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+            </form>
         </div>
     );
 };
