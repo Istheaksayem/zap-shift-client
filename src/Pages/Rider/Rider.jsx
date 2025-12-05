0dde262a9118d15cba47f850e3cbe6ff92a1a415
Istheaksayem <istheakahmed9345@gmail.com>
2025-12-05 14:00:18 +0600

    Save Rider data in the database

--- src/Pages/Rider/Rider.jsx
+++ src/Pages/Rider/Rider.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { useForm, useWatch } from 'react-hook-form';
 import UseAuth from '../../Hooks/UseAuth';
 import useAxiosSecure from '../../Hooks/useAxiosSecure';
-import {  useLoaderData } from 'react-router';
+import { useLoaderData } from 'react-router';
+import Swal from 'sweetalert2';
 
 const Rider = () => {
     const {
@@ -28,43 +29,55 @@ const Rider = () => {
         return districts;
     }
 
-    const senderRegion = useWatch({ control, name: 'senderRegion' })
+    const riderRegion = useWatch({ control, name: 'region' })
 
     const handleRiderApplication = data => {
         console.log(data)
+        axiosSecure.post('/riders', data)
+            .then(res => {
+                if (res.data.insertedId) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Your application has been submitted.we will reach to you in 15 Days",
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+                }
+            })
     }
     return (
         <div>
             <h2 className="text-4xl text-primary">Be a Rider</h2>
             <form onSubmit={handleSubmit(handleRiderApplication)} className='mt-12 p-4 text-black overflow-x-hidden'>
-             
+
                 {/* two column */}
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
 
-                    {/* LEFT SIDE — Sender Details */}
+                    {/*rider Details */}
                     <div>
                         <fieldset className="fieldset">
                             <h4 className="text-2xl font-semibold mb-2">Rider Details</h4>
-                            {/* sender Name  */}
+                            {/* rider Name  */}
 
-                            <label className="label">Sender Name</label>
+                            <label className="label">Rider Name</label>
                             <input
                                 type="text"
-                                {...register('senderName')}
+                                {...register('name')}
                                 defaultValue={user?.displayName}
-                                className="input w-full" placeholder="Sender Name" />
-                            {/* sender Email  */}
+                                className="input w-full" placeholder="Rider Name" />
+                            {/* rider Email  */}
 
-                            <label className="label">Sender Email</label>
+                            <label className="label">Rider Email</label>
                             <input
                                 type="email"
-                                {...register('senderEmail')}
+                                {...register('riderEmail')}
                                 defaultValue={user?.email}
-                                className="input w-full" placeholder="Sender Email" />
-                            {/* sender Region */}
+                                className="input w-full" placeholder="Rider Email" />
+                            {/* rider Region */}
                             <fieldset className="fieldset">
-                                <legend className="fieldset-legend">Sender Region</legend>
-                                <select {...register('senderRegion')} defaultValue="Pick a region" className="select">
+                                <legend className="fieldset-legend">Region</legend>
+                                <select {...register('region')} defaultValue="Pick a region" className="select">
                                     <option disabled={true}>Pick a region</option>
 
                                     {
@@ -74,43 +87,46 @@ const Rider = () => {
 
                             </fieldset>
 
-                            {/* sender Districts */}
+                            {/* rider Districts */}
                             <fieldset className="fieldset">
-                                <legend className="fieldset-legend">Sender district</legend>
-                                <select {...register('senderDistrict')} defaultValue="Pick a district" className="select">
+                                <legend className="fieldset-legend">district</legend>
+                                <select {...register('district')} defaultValue="Pick a district" className="select">
                                     <option disabled={true}>Pick a districts</option>
 
                                     {
-                                        districtsByRegion(senderRegion).map((r, i) => <option key={i} value={r}>{r}</option>)
+                                        districtsByRegion(riderRegion).map((r, i) => <option key={i} value={r}>{r}</option>)
                                     }
 
                                 </select>
-                                {
-                                    console.log(districtsByRegion(senderRegion))
-                                }
+                                {/* {
+                                    console.log(districtsByRegion(riderRegion))
+                                } */}
 
                             </fieldset>
 
+                            {/* rider Address  */}
+
+                            <label className="label mt-4">Your Address</label>
+                            <input type="text" {...register('Address')} className="input w-full" placeholder="rider Address" />
 
 
 
-                            {/* sender Address  */}
+                            <label className="label mt-4">rider Contact</label>
+                            <input type="number" {...register('riderContact')} className="input w-full" placeholder="rider Contact" />
 
-                            <label className="label mt-4">Sender Address</label>
-                            <input type="text" {...register('senderAddress')} className="input w-full" placeholder="Sender Address" />
+                            <label className="label">Driving License </label>
+                            <input
+                                type="text"
+                                {...register('license')}
 
+                                className="input w-full" placeholder="Driving License" />
 
+                            <label className="label mt-4">NID</label>
+                            <input type="number" {...register('nid')} className="input w-full" placeholder="NID" />
+                            {/* BIKE INFORMATION */}
+                            <label className="label mt-4">BIKE INFORMATION</label>
+                            <input type="text" {...register('bike')} className="input w-full" placeholder="BIKE INFORMATION" />
 
-                            <label className="label mt-4">Sender Contact</label>
-                            <input type="number" {...register('senderContact')} className="input w-full" placeholder="Sender Contact" />
-
-                            <label className="label mt-4">Sender Note</label>
-                            <textarea
-                                {...register('senderNote')}
-                                className="textarea w-full"
-                                placeholder="Write sender notes..."
-                                rows={4}
-                            ></textarea>
                         </fieldset>
                     </div>
 
@@ -119,10 +135,17 @@ const Rider = () => {
                 </div>
 
 
-                <input type="submit" className='btn btn-primary mt-8 text-black' value="send parcel" />
+                <input type="submit" className='btn btn-primary mt-8 text-black' value="Apply as a rider" />
             </form>
         </div>
     );
 };
 
 export default Rider;
+
+{/* <textarea
+    {...register('riderNote')}
+    className="textarea w-full"
+    placeholder="Write rider notes..."
+    rows={4}
+></textarea> */}
